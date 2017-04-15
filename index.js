a9992fd86d58ed711d5515c0041d4d192d73dd1b
Konstanza <ktm2295@hotmail.com>
2017-04-15 01:46:47 -0400

    New facebook login
    - Page loading changed

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 
 const electron = require('electron');  
 const app = electron.app;  
+const ipc = electron.ipcMain;
 const BrowserWindow = electron.BrowserWindow;
 const winWidth = 800; //1024
 const winHeight = 600; //768
+
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+const FB = require('fb');
 
 let mainWindow;
 
@@ -27,5 +32,30 @@ app.on('ready', function() {
   });
 
   mainWindow.setSize(winWidth,winHeight); // Black screen error fixed
-      
+  
+  ipc.on("facebook-button-clicked",function (event, arg) {
+    var options = {
+      client_id: '271293853330276',
+      scopes: "public_profile",
+      redirect_uri: "https://www.facebook.com/connect/login_success.html"
+    };
+    var authWindow = new BrowserWindow({ width: 450, height: 300, show: false, 'node-integration': false });
+    var facebookAuthURL = "https://www.facebook.com/dialog/oauth?client_id=" + options.client_id + "&redirect_uri=" + options.redirect_uri + "&response_type=token,granted_scopes&scope=" + options.scopes + "&display=popup";
+    authWindow.loadURL(facebookAuthURL);
+    authWindow.show();
+    authWindow.webContents.on('did-get-redirect-request', function (event, oldUrl, newUrl) {
+      var raw_code = /access_token=([^&]*)/.exec(newUrl) || null;
+      var access_token = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
+      var error = /\?error=(.+)$/.exec(newUrl);
+      if(access_token) {
+        FB.setAccessToken(access_token);
+        FB.api('/me', { fields: ['id', 'name', 'picture.width(150).height(150)'] }, function (res) {
+          mainWindow.webContents.executeJavaScript("document.getElementById(\"facebook-name\").innerHTML = \" " + res.name + "\"");
+          //mainWindow.webContents.executeJavaScript("document.getElementById(\"fb-id\").innerHTML = \" ID: " + res.id + "\"");
+          mainWindow.webContents.executeJavaScript("document.getElementById(\"facebook-profile-picture\").src = \"" + res.picture.data.url + "\"");
+        });
+        authWindow.close();
+      }
+    });
+  });
 });
